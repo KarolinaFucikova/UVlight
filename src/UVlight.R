934af7596bf108dbfacb76e4157923836627cd62
Karolina Fucikova <karolina.fucikova@gmail.com>
2022-02-11 23:13:17 -0500

    Commit to accommodate original data format

--- src/UVlight.R
+++ src/UVlight.R
@@ -1,6 +1,31 @@
 # read in data from a file in that directory 
 
-UVdata <- read.csv(file = "data/BOL5-4data.csv")
+UVdata <- read.csv(file = "data/BOL5-4data2.csv")
+# there may be some extra columns and rows, so just get the ones needed
+# the following gets rows 1-8 and columns 1-5
+UVdata <- UVdata[c(1:8),c(1:5)]
+#names(UVdata) <- as.character(c("treatment","UV24","UV48","UV96","UV144"))
+names(UVdata) <- as.character(c("treatment","24","48","96","144"))
+
+# if the table is in the "wide" format, we need to transform it first
+# from wide shape to long shape; tidyr package is having a lot of trouble with the column headings
+# tutorial here: datasciencemadesimple.com/reshape-in-r-from-wide-to-long-from-long-to-wide/
+
+#UVdata<- reshape(data=UVdata, idvar="treatment",
+#                         varying = c("UV24","UV48","UV96","UV144"),
+#                         v.name=c("Survival"),
+#                         times=c("UV24","UV48","UV96","UV144"),
+#                         new.row.names = 1:1000,
+#                         direction="long")
+
+UVdata<- reshape(data=UVdata, idvar="treatment",
+                 varying = c("24","48","96","144"),
+                 v.name=c("Survival"),
+                 times=c("24","48","96","144"),
+                 new.row.names = 1:1000,
+                 direction="long")
+
+names(UVdata) <- c("treatment","Uvlevel", "Survival")
 
 names(UVdata)
 # this just shows you the names of your variables, so that you can correctly enter them into the analysis script below
@@ -14,9 +39,12 @@ boxplot(Survival~Uvlevel*treatment, data=UVdata)
 # setting the colors
 colors <- c("orange", "turquoise")
 # plotting
+# annoyingly, the uv levels are alphabetized now because of the table transformation
 boxplot(Survival~treatment*Uvlevel, data=UVdata, col=colors, xlab = "UV-C level (J/m2)", ylab = "% survival")
 legend("topright", inset=.02, title="Treatment",
        c("dark","light"), fill=colors, horiz=TRUE, cex=0.8)
+
+# I can fix that but not sure it's worth the trouble since these aren't figures to be used anyway
 
 # now the actual ANOVA - set it up similarly to setting up the box plot
 UV.aov = aov(Survival~treatment*Uvlevel, data=UVdata)
@@ -37,6 +65,13 @@ TukeyHSD(Uv.aov2, ordered = TRUE, which = "fUvlevel", conf.level = 0.95)
 
 #######
 # trying some more graphing
+# problem now is the values of UV levels are treated as text, not numbers
+# fix that
+UVdata$Uvlevel <- as.numeric(UVdata$Uvlevel)
+
+# load ggplot package
+library(ggplot2)
+# plot
 fancyplot<-ggplot(UVdata, aes(x=Uvlevel, y=Survival, color=treatment, shape=treatment)) + 
   geom_point(size=5) + geom_smooth(method=lm, aes(fill=treatment))
 fancyplot 
